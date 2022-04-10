ca876521378e8b0e8c86808e4caaf0c104e14a7d
marekParek <48125221+fadrny@users.noreply.github.com>
2022-04-10 17:43:43 +0200

    Add project files.

---
+++ Kadel/KadelBot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Kadel
+{
+    internal class KadelBot
+    {
+        private Point _position;
+
+        private int _direction;
+        
+        public Point Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+        public int Direction
+        {
+            get { return _direction; }
+        }
+
+        public KadelBot(Point pozice)
+        {
+            _position = pozice;
+            _direction = 0;
+        }
+
+        public void ChangeDirection()
+        {
+            _direction += 1;
+            if (_direction > 3)
+                _direction = 0;
+        }
+        public ImageBrush GetDirectionImage()
+        {
+            ImageBrush imgBrush = new ImageBrush();
+            
+            switch (_direction)
+            {      
+                case 0:
+                    imgBrush.ImageSource = new BitmapImage(new Uri("../../../images/vpravo.png", UriKind.Relative));
+                    break;
+                case 1:
+                    imgBrush.ImageSource = new BitmapImage(new Uri("../../../images/nahoru.png", UriKind.Relative));
+                    break;
+                case 2:
+                    imgBrush.ImageSource = new BitmapImage(new Uri("../../../images/vlevo.png", UriKind.Relative));
+                    break;
+                case 3:
+                    imgBrush.ImageSource = new BitmapImage(new Uri("../../../images/dolu.png", UriKind.Relative));
+                    break;
+                default:
+                    imgBrush.ImageSource = new BitmapImage(new Uri("../../../images/vpravo.png", UriKind.Relative));
+                    break;
+            }
+            return imgBrush;
+        }
+        
+        
+    }
+}
---
+++ Kadel/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kadel
+{  
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Rectangle[,] rect = new Rectangle[10, 10];
+        KadelBot kadelBot = new KadelBot(new Point(9, 0));
+        
+        void DrawIt()
+        {
+            rect[(int)kadelBot.Position.X, (int)kadelBot.Position.Y].Fill = kadelBot.GetDirectionImage();
+
+            ug_hraci_plocha.Children.Clear();
+            foreach (Rectangle r in rect)
+            {
+                ug_hraci_plocha.Children.Add(r);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int x = 0; x < rect.GetLength(0); x++)
+            {
+                for (int y = 0; y < rect.GetLength(1); y++)
+                {
+                    Rectangle r = new Rectangle();
+                    r.Stroke = Brushes.Black;
+                    
+
+                    rect[x, y] = r;
+                    ug_hraci_plocha.Children.Add(r);
+                }
+            }
+        }
+
+        private void btn_krok_Click(object sender, RoutedEventArgs e)
+        {
+            DrawIt();
+        }
+    }
+}
